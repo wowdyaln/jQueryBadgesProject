04fa9eb347af68db2aad0a14b73837bd80d3a57f
wowdyaln <rogersmk@gmail.com.tw>
2016-12-09 19:58:06 +0800

    Adding Course Buttons

--- assets/main.js
+++ assets/main.js
@@ -27,6 +27,14 @@ $(function() {
         src: course.badge
       }).appendTo($course);
 
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        herf: course.url,
+        text: 'see course'
+      }).appendTo($course); //按鈕案下去無法連結？
+
+
     })
   }
 
